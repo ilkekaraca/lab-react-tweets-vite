29c8836a13673e4c9cad094a6097c5950cd2d168
“Ilke <ilkedidem@gmail.com>
2023-05-11 23:01:29 +0200

    done

---
+++ src/components/Messages.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Messages({ message }) {
+  return <p className="message"> {message} </p>;
+}
---
+++ src/components/TimeStamp.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TimeStamp({ time }) {
+  return <span className="timestamp"> {time} </span>;
+}
